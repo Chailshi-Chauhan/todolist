26323cc803161261cddff6e7f0fd0b0e866c5b5b
Chailshi chauhan <chailshichauhan2000@gmail.com>
2020-08-06 17:55:39 +0530

    Updates

---
+++ precache-manifest.72d48f56e435ae81065d2f928a5e8e7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fce688d613ce74cfabc441c387c1545",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/todolist/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "5fae2a27eee8cda3360b",
+    "url": "/todolist/static/js/main.f5425395.chunk.js"
+  },
+  {
+    "revision": "ad7578fd4deaca32df67",
+    "url": "/todolist/static/js/runtime-main.8f9906f5.js"
+  }
+]);
